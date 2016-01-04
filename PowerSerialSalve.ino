080cf2f0e526ae93e0ef3769912079d2c3f47bac
Pete Darling <pete.darling@digivate.com>
2016-01-04 21:26:10 +0000

    new version 0.0.1.3.  Made readngs more stable

--- PowerSerialSalve.ino
+++ PowerSerialSalve.ino
@@ -4,7 +4,7 @@
 *  RX is connected to Mega serial 1 TX
 *  TX is connected to Mega serial 1 RX
 *
-*  Version 0.0.1.2
+*  Version 0.0.1.3
 */
 
 /**
@@ -28,33 +28,66 @@ const unsigned long readPowerInterval = 10000;//read every 2.5 sec, calcVI times
 unsigned long readPowerMillis = 0; // last milli secs since last avg reading
 typedef struct { float power, Vrms, Irms;} PayloadTX;      // create structure - a neat way of packaging data for RF comms
 PayloadTX emontx;  
+PayloadTX powerAvg;  
+int avgCount = 0;
+const int avgTimes = 10;
 
 void setupPower()
 {
-  ACsensor.current(1, 121.5);//60.606);    //111.1  //155.5
+  ACsensor.current(1, 138.6);//60.606);    //111.1  //155.5
   ACsensor.voltage(0, 256.97, 1.7);   //268.97
   //calc power a few times to level out
   for(int i=0; i<10; i++) {
-    ACsensor.calcVI(30, 2000);  // Calculate 
-  
+    ACsensor.calcVI(30, 2000);  // Calculate   
   }
 }
 
-void readPower() {
-   led.on();
+void readPower2() {
+   
    ACsensor.calcVI(60, 3000);  // Calculate 
    emontx.power = ACsensor.realPower;
-   if (emontx.power < 1.000) emontx.power = 0.00;
+   if (emontx.power < 1.9) emontx.power = 0.00;
    emontx.Vrms = ACsensor.Vrms;//*100;    
    emontx.Irms = ACsensor.Irms;    
-   if (emontx.power < 1.000) emontx.Irms = 0.00;
-   led.off();
+   if (emontx.power < 1.9) emontx.Irms = 0.00;
+   
+   sendPower();
+}
+
+void readPower() {
+  
+   ACsensor.calcVI(60, 3000);  // Calculate 
+  powerAvg.Vrms += ACsensor.Vrms;//*100;    
+  if (ACsensor.realPower>1.9) { // sensor can not go lower then 2W as using 100A CT, have to work on that or get a smaller CT sensor
+   powerAvg.power += ACsensor.realPower;   
+   powerAvg.Irms += ACsensor.Irms;    
+   }
+   avgCount++;
+   
+   if (avgCount==avgTimes) {     
+     emontx.power = powerAvg.power/avgTimes;
+     if (emontx.power < 2) emontx.power = 0;
+     emontx.Vrms = powerAvg.Vrms/avgTimes;//*100;    
+     emontx.Irms = powerAvg.Irms/avgTimes;    
+     if (emontx.power < 2) emontx.Irms = 0;
+     sendPower();
+     resetAvg();
+   }
+
+  
+}
+
+void resetAvg() {
+   powerAvg.power = 0.00;
+   powerAvg.Vrms = 0.00;    
+   powerAvg.Irms = 0.00;
+   avgCount = 0;
 }
 
 char buf[100];
 
 void sendPower() {
-  
+  led.on();
   String tempStr = String(emontx.power);//map float to a string
   char powerBuf[24];
   tempStr.toCharArray(powerBuf, 24);//map float to char array
@@ -78,7 +111,8 @@ void sendPower() {
   if (DEBUG_TO_SERIAL==1) {
     Serial.println(buf);
   }
-  delay(100);  
+  led.off();
+ // delay(100);  
 }
 
 void setup() {
@@ -94,7 +128,7 @@ void setup() {
 
 void loop() {
   readPower();
-  sendPower();
+ // sendPower();
 }
 
 
